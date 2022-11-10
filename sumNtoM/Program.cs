e9a52934ca2d55439b965bc506da261704e839cb
Ahmet Aksünger <116587797+AhmetAksunger@users.noreply.github.com>
2022-11-10 20:55:26 +0300

    Add files via upload

---
+++ sumNtoM/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumNtoM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sumNtoM(2, 10);
+            sumNtoM(10, 2);
+            Console.ReadLine();
+        }
+        static void sumNtoM(int n, int m)
+        {
+            
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter the starting number: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the last number");
+                m = Convert.ToInt32(Console.ReadLine());
+                if (n > m)
+                {
+                    sumNtoM(m, n);
+                }
+                for (int i = n; i <= m; i++)
+                {
+                    sum = sum + i;
+                }
+                if (m > n)
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+            
+
+        }
+    }
+}
